4dbe203d5bda8641ab73b9f6635bf6694dfd5f63
Mark Jones <mark.jones@telethonkids.org.au>
2019-05-31 14:38:06 +0800

    null case plus updated cfg

--- sim_script_generator.R
+++ sim_script_generator.R
@@ -1,12 +1,12 @@
 library(dplyr)
 
-nsim <- 1000  # 10000
+nsim <- 500  # 10000
 accrual <- c(30, 50) 
 info_delay <- c(0.5, 0.7)
 p0 <- c(0.1, 0.4, 0.7)
 pdelta <- c(0.05, 0.1, 0.15) # 0
 t0 <- c(20, 35, 50)
-tdelta <-  c(5, 10, 15) # 0
+tdelta <- c(5, 10, 15) # 0
 
 d <- expand.grid(accrual, info_delay, 
                  p0, pdelta, 
